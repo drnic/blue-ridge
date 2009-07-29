319162728220498da8310699b7ace9403c4799e8
Dr Nic Williams <drnicwilliams@gmail.com>
2009-07-29 09:49:47 +0200

    fixed fixture for dom transactions

--- spec/javascripts/screw.dom_transactions_spec.js
+++ spec/javascripts/screw.dom_transactions_spec.js
@@ -1,14 +1,14 @@
 Screw.Unit(function() {
   describe("DOM", function(){
     it("should be the default DOM initially", function(){
-      expect($('div#some_dom p').size()).to(equal, 1);
+      expect($('div#some_dom p').size()).to(equal, 2);
     });
     it("should be modifiable by one test", function(){
-      $('div#some_dom p').remove();
-      expect($('div#some_dom p').size()).to(equal, 0);
+      $('div#some_dom p:first').remove();
+      expect($('div#some_dom p').size()).to(equal, 1);
     });
     it("should be restored before this test", function(){
-      expect($('div#some_dom p').size()).to(equal, 1);
+      expect($('div#some_dom p').size()).to(equal, 2);
     });
   });
 });
